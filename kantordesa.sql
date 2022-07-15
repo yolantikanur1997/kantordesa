083f41e56a403f819deaa61bcc6d819d4005b0c6
yolantika <yolantika@indoskyware.com>
2022-07-15 19:35:08 +0700

    belum fix

--- kantordesa.sql
+++ kantordesa.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 18 Jun 2022 pada 10.09
+-- Waktu pembuatan: 15 Jul 2022 pada 12.33
 -- Versi server: 10.4.24-MariaDB
--- Versi PHP: 7.4.29
+-- Versi PHP: 8.1.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,174 +26,40 @@ SET time_zone = "+00:00";
 --
 -- Struktur dari tabel `admin`
 --
-
-CREATE TABLE `admin` (
-  `id_admin` int(11) NOT NULL,
-  `nama` varchar(70) NOT NULL,
-  `username` varchar(70) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `admin`
---
-
-INSERT INTO `admin` (`id_admin`, `nama`, `username`, `password`) VALUES
-(0, 'Yolantika Nur', 'yoyo', '$2y$10$uKsrfxFI1LwvBmVD1zimSeXZuISSU61IcVRT0EbHSoRaKsMKF2gp6');
+-- Kesalahan membaca struktur untuk tabel kantordesa.admin: #1932 - Table &#039;kantordesa.admin&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantordesa.admin: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantordesa`.`admin`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `berkas`
 --
-
-CREATE TABLE `berkas` (
-  `id_berkas` int(11) NOT NULL,
-  `id_pengguna` int(11) NOT NULL,
-  `jenis_berkas` varchar(30) NOT NULL,
-  `nama_berkas` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `berkas`
---
-
-INSERT INTO `berkas` (`id_berkas`, `id_pengguna`, `jenis_berkas`, `nama_berkas`) VALUES
-(10, 1, 'KTP', '35314244.png'),
-(13, 1, 'Surat Pengantar RT', '33289965.jpeg'),
-(14, 7, 'KTP', '38183934.png');
+-- Kesalahan membaca struktur untuk tabel kantordesa.berkas: #1932 - Table &#039;kantordesa.berkas&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantordesa.berkas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantordesa`.`berkas`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `jenis_surat`
 --
-
-CREATE TABLE `jenis_surat` (
-  `id_jenis_surat` int(11) NOT NULL,
-  `nama_surat` varchar(100) NOT NULL,
-  `keterangan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `jenis_surat`
---
-
-INSERT INTO `jenis_surat` (`id_jenis_surat`, `nama_surat`, `keterangan`) VALUES
-(3, 'Surat Pengantar Catatan Kepolisian', 'lorem ipsum ipsum'),
-(4, 'Surat Pernyataan Tanah', 'lorem ipsum');
+-- Kesalahan membaca struktur untuk tabel kantordesa.jenis_surat: #1932 - Table &#039;kantordesa.jenis_surat&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantordesa.jenis_surat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantordesa`.`jenis_surat`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `pengajuan_surat`
 --
-
-CREATE TABLE `pengajuan_surat` (
-  `id_pengguna_surat` int(11) NOT NULL,
-  `id_pengguna` int(11) NOT NULL,
-  `id_jenis_surat` int(11) NOT NULL,
-  `tanggal_pengajuan` date NOT NULL,
-  `keperluan` text NOT NULL,
-  `komentar` text NOT NULL,
-  `status` varchar(70) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- Kesalahan membaca struktur untuk tabel kantordesa.pengajuan_surat: #1932 - Table &#039;kantordesa.pengajuan_surat&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantordesa.pengajuan_surat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantordesa`.`pengajuan_surat`&#039; at line 1
 
 -- --------------------------------------------------------
 
 --
 -- Struktur dari tabel `pengguna`
 --
-
-CREATE TABLE `pengguna` (
-  `id_pengguna` int(11) NOT NULL,
-  `no_kk` varchar(16) NOT NULL,
-  `nik` varchar(16) NOT NULL,
-  `nama` varchar(70) NOT NULL,
-  `tempat_lahir` varchar(50) NOT NULL,
-  `tanggal_lahir` date NOT NULL,
-  `jenis_kelamin` varchar(25) NOT NULL,
-  `alamat` text NOT NULL,
-  `rt` int(11) NOT NULL,
-  `rw` int(11) NOT NULL,
-  `kelurahan` varchar(100) NOT NULL,
-  `kecamatan` varchar(100) NOT NULL,
-  `agama` varchar(30) NOT NULL,
-  `status_perkawinan` varchar(30) NOT NULL,
-  `pekerjaan` varchar(50) NOT NULL,
-  `kewarganegaraan` varchar(30) NOT NULL,
-  `password` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data untuk tabel `pengguna`
---
-
-INSERT INTO `pengguna` (`id_pengguna`, `no_kk`, `nik`, `nama`, `tempat_lahir`, `tanggal_lahir`, `jenis_kelamin`, `alamat`, `rt`, `rw`, `kelurahan`, `kecamatan`, `agama`, `status_perkawinan`, `pekerjaan`, `kewarganegaraan`, `password`) VALUES
-(1, '12345678', '12345', 'Yolantika Nur', 'Sanggau', '2018-12-31', 'Laki-Laki', 'Pontianak', 1, 2, 'Bansir Darat', 'Pontianak Tenggara', 'Islam', 'Belum Kawin', 'Swasta', 'WNI', '$2y$10$UG3kZpiyGw5FlESnt/tcD.8XiQElBbV8qX.1/peJKZumou5UUuMX.'),
-(7, '12345', '12345678', 'YBRevolution', 'Sanggau', '2022-06-17', 'Laki-Laki', 'Sanggau', 5, 5, 'Beringin', 'Kapuas', 'Islam', 'Belum Kawin', 'Swasta', 'WNI', '$2y$10$ZiivfFlWMS.RWt.646a8zOMSzcglEs9k5AztbxYaF3nqNb7sTNQq.');
-
---
--- Indexes for dumped tables
---
-
---
--- Indeks untuk tabel `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id_admin`);
-
---
--- Indeks untuk tabel `berkas`
---
-ALTER TABLE `berkas`
-  ADD PRIMARY KEY (`id_berkas`);
-
---
--- Indeks untuk tabel `jenis_surat`
---
-ALTER TABLE `jenis_surat`
-  ADD PRIMARY KEY (`id_jenis_surat`);
-
---
--- Indeks untuk tabel `pengajuan_surat`
---
-ALTER TABLE `pengajuan_surat`
-  ADD PRIMARY KEY (`id_pengguna_surat`);
-
---
--- Indeks untuk tabel `pengguna`
---
-ALTER TABLE `pengguna`
-  ADD PRIMARY KEY (`id_pengguna`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
---
-
---
--- AUTO_INCREMENT untuk tabel `berkas`
---
-ALTER TABLE `berkas`
-  MODIFY `id_berkas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- AUTO_INCREMENT untuk tabel `jenis_surat`
---
-ALTER TABLE `jenis_surat`
-  MODIFY `id_jenis_surat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT untuk tabel `pengajuan_surat`
---
-ALTER TABLE `pengajuan_surat`
-  MODIFY `id_pengguna_surat` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT untuk tabel `pengguna`
---
-ALTER TABLE `pengguna`
-  MODIFY `id_pengguna` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+-- Kesalahan membaca struktur untuk tabel kantordesa.pengguna: #1932 - Table &#039;kantordesa.pengguna&#039; doesn&#039;t exist in engine
+-- Kesalahan membaca data untuk tabel kantordesa.pengguna: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `kantordesa`.`pengguna`&#039; at line 1
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
